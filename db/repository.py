eced46fc882aa7cb9c23d76bff8360ced1e4e232
mox <gromovs252@gmail.com>
2026-09-09 10:09:54 +0300

    Update repository.py

--- db/repository.py
+++ db/repository.py
@@ -2,10 +2,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
 from datetime import datetime, timedelta
 
-from ..schemas.accounts import AccountCreate, AccountResponse
-from ..db.models import AccountPool, AccountStatuses
+from ..schemas.accounts import AccountCreate, TaskCreate
+from ..db.models import AccountPool, AccountStatuses, SearchTask, TasksTypes, TasksStatuses
 
-async def add_account(db: AsyncSession, user: AccountCreate) -> AccountPool:
+async def add_account(db: AsyncSession, user: AccountCreate) -> AccountPool | None:
     db_acc = await get_account_by_username(db, user.username)
     if db_acc is not None:
         raise ValueError(f"Account with username: {user.username} is already exists")
@@ -13,7 +13,7 @@ async def add_account(db: AsyncSession, user: AccountCreate) -> AccountPool:
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
-    return account
+    return new_user
     
 async def get_account_by_username(db: AsyncSession, username: str) -> AccountPool | None:
     account = (await db.execute(select(AccountPool).where(AccountPool.username == username))).scalar_one_or_none()
@@ -22,15 +22,30 @@ async def get_account_by_username(db: AsyncSession, username: str) -> AccountPoo
 async def get_available_account(db: AsyncSession) -> AccountPool | None:
     cooldown = datetime.now(timezone.utc) - timedelta(minutes=30)
     accounts = (await db.execute(select(AccountPool)
+    .where(AccountPool.status == AccountStatuses.AVAILABLE)
     .where((AccountPool.last_used_at == None) |
     (AccountPool.last_used_at < cooldown)).order_by(AccountPool.last_used_at.asc()).limit(1))).scalar_one_or_none()
     return accounts
     
-async def update_account_status(db: AsyncSession, account: AccountPool, status: AccountStatuses) -> AccountResponse:
+async def update_account_status(db: AsyncSession, account: AccountPool, status: AccountStatuses) -> None:
     await db.execute(update(AccountPool).where(AccountPool.id == account.id).values(status = status))
     await db.commit()
     
 async def update_last_used(db: AsyncSession, account_id: int) -> None:
     await db.execute(update(AccountPool).where(AccountPool.id==account_id).values(last_used_at=datetime.now(timezone.utc)))
-	await db.commit()
+    await db.commit()
 
+async def increment_parsed_count(db: AsyncSession, account_id: int) -> None:
+    await db.execute(update(AccountPool).where(AccountPool.id == account_id).values(profiles_parsed_count=AccountPool.profiles_parsed_count+1))
+    await db.commit()
+    
+async def list_accounts(db: AsyncSession) -> list[AccountPool]:
+    accounts = (await db.execute(select(AccountPool))).scalars().all()
+    return accounts
+
+async def create_task(db: AsyncSession, task: TaskCreate) -> SearchTask:
+    new_task = SearchTask(**task.model_dump())
+    db.add(new_task)
+    await db.commit()
+    await db.refresh(new_task)
+    return new_task
