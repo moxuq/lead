996da22578bcbce954b0832ce5f922910fa3d775
mox <gromovs252@gmail.com>
2026-09-09 09:52:07 +0300

    Update repository.py

--- db/repository.py
+++ db/repository.py
@@ -1,27 +1,36 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
+from datetime import datetime, timedelta
 
 from ..schemas.accounts import AccountCreate, AccountResponse
 from ..db.models import AccountPool, AccountStatuses
 
 async def add_account(db: AsyncSession, user: AccountCreate) -> AccountPool:
+    db_acc = await get_account_by_username(db, user.username)
+    if db_acc is not None:
+        raise ValueError(f"Account with username: {user.username} is already exists")
     new_user = AccountPool(**user.model_dump())
     db.add(new_user)
     await db.commit()
-    db.refresh(new_user)
-    return AccountResponse.model_validate(new_user)
+    await db.refresh(new_user)
+    return account
     
 async def get_account_by_username(db: AsyncSession, username: str) -> AccountPool | None:
     account = (await db.execute(select(AccountPool).where(AccountPool.username == username))).scalar_one_or_none()
-    return AccountResponse.model_validate(account)
+    return account
 
-async def get_available_account(db: AsyncSession) -> list[AccountPool] | None:
-    accounts = (await db.execute(select(AccountPool).where(AccountPool.status == AccountStatuses.AVAILABLE))).scalars().all()
+async def get_available_account(db: AsyncSession) -> AccountPool | None:
+    cooldown = datetime.now(timezone.utc) - timedelta(minutes=30)
+    accounts = (await db.execute(select(AccountPool)
+    .where((AccountPool.last_used_at == None) |
+    (AccountPool.last_used_at < cooldown)).order_by(AccountPool.last_used_at.asc()).limit(1))).scalar_one_or_none()
     return accounts
     
 async def update_account_status(db: AsyncSession, account: AccountPool, status: AccountStatuses) -> AccountResponse:
-    await db.execute(update(AccountPool).where(AccountPool.id == account.id).values(AccountPool.status = status))
+    await db.execute(update(AccountPool).where(AccountPool.id == account.id).values(status = status))
     await db.commit()
     
-async def update_last_used(db: AsyncSession):
-    pass
+async def update_last_used(db: AsyncSession, account_id: int) -> None:
+    await db.execute(update(AccountPool).where(AccountPool.id==account_id).values(last_used_at=datetime.now(timezone.utc)))
+	await db.commit()
+
