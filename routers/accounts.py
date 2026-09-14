5ede20fdfcea764a7166c42b33c011c51bd1d389
mox <gromovs252@gmail.com>
2026-09-14 17:46:06 +0300

    Update accounts.py

--- routers/accounts.py
+++ routers/accounts.py
@@ -1,14 +1,17 @@
 from ..db.database import get_db
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession, APIRouter
 from ..schemas.accounts import AccountResponse, AccountCreate
 from ..db.repository import add_account, list_accounts
 
 accounts_router = APIRouter(prefix='/accounts', tags=['Account'])
 
-@accounts_router.post('/', response_model=AccountResponse)
+@accounts_router.post('/', response_model=AccountResponse, status_code=201)
 async def post_create_account(new_account: AccountCreate, db: AsyncSession = Depends(get_db)):
-    result = await add_account(db, new_account)
+    try:
+        result = await add_account(db, new_account)
+    except ValueError:
+        raise HTTPException(status_code=400, detail='Account is already exists')
     return result
 
 @accounts_router.get('/', response_model=list[AccountResponse])
