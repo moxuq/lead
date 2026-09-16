c2c985e3f6f473bc5854fec5d946a9b90f9b018e
mox <gromovs252@gmail.com>
2026-09-16 12:19:46 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from .routers import (
+    accounts,
+    tasks,
+    leads,
+    stats,
+    error
+)
+
+app = FastAPI(title="Lead", version="1.0.0")
+app.include_router(accountss.accounts_router)
+app.include_router(tasks.tasks_router)
+app.include_router(leads.leads_router)
+app.include_router(stats.stats_router)
+app.include_router(error.errors_router)
+
