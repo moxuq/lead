35caff9f4728f0d19b3b6b5e9e079d37e053e6f4
mox <gromovs252@gmail.com>
2026-09-16 12:12:35 +0300

    Update error.py

--- routers/error.py
+++ routers/error.py
@@ -2,8 +2,11 @@ from fastapi import APIRouter, Depends
 
 from ..db.repository import list_errors
 from ..db.models import ErrorLog
+from ..db.database import get_db
 
 errors_router = APIRouter(prefix='/errors', tags=['Errors'])
 
 @errors_router.get('/', response_model=list[ErrorLog])
-async def get_list_of_errors(offset: int = 20, 
+async def get_list_of_errors(limit: int = 20, db: AsyncSession = Depends(get_db)):
+    results = await list_errors(db, limit)
+    return results
