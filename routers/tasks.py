44615d685318926633573eeece65cba077ec10d6
mox <gromovs252@gmail.com>
2026-09-16 16:45:33 +0300

    Update tasks.py

--- routers/tasks.py
+++ routers/tasks.py
@@ -1,8 +1,9 @@
 from fastapi import Depends, APIRouter, HTTPException
+from pydantic import BaseModel
 
 from ..schemas.tasks import TaskCreate, TaskResponse
 from ..db.database import get_db
-from ..db.repository import create_task, list_tasks, get_task, update_task_status
+from ..db.repository import create_task, list_tasks, get_task, update_task_status, list_tasks_by_status
 from ..db.models import TasksStatuses
 
 tasks_router = APIRouter(prefix='/tasks', tags=['Tasks'])
@@ -14,7 +15,7 @@ async def post_create_new_task(new_task: TaskCreate, db: AsyncSession = Depends(
     
 @tasks_router.get('/', response_model=list[TaskResponse])
 async def get_list_of_tasks(status: TasksStatuses | None = None, db: AsyncSession = Depends(get_db)):
-   result = await list_tasks(db)
+   result = await list_tasks_by_status(db, status)
    return result
 
 @tasks_router.get('/{task_id}', response_model=TaskResponse)
@@ -24,10 +25,13 @@ async def get_task_by_id(task_id: int, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=404, detail=f'Task {task_id} not found')
     return result
 
+class UpdateStatusRequest(BaseModel):
+   status: TasksStatuses
+
 @tasks_router.put('/{task_id}/status')
-async def put_status_by_task_id(task_id: int, status: TasksStatuses, db: AsyncSession = Depends(get_db)):
+async def put_status_by_task_id(task_id: int, data: UpdateStatusRequest, db: AsyncSession = Depends(get_db)):
     task = await get_task(db, task_id)
     if task is None:
         raise HTTPException(status_code=404, detail=f'Task {task_id} not found')
-    await update_task_status(db, task_id, status)
+    await update_task_status(db, task_id, data.status)
     return {"message": "Status updated"}
