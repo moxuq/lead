9b2510f2460db527682bfa34da4bf17231f0529d
mox <gromovs252@gmail.com>
2026-09-16 16:36:35 +0300

    Fix main.py

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ from .routers import (
 )
 
 app = FastAPI(title="Lead", version="1.0.0")
-app.include_router(accountss.accounts_router)
+app.include_router(accounts.accounts_router)
 app.include_router(tasks.tasks_router)
 app.include_router(leads.leads_router)
 app.include_router(stats.stats_router)
 app.include_router(error.errors_router)
-
